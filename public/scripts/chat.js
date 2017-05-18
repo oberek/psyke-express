527b90091226f5bab5d2ec05a77b5bfaa45ce2ca
Thomas Feuerborn <tfeuerbo@gmail.com>
2017-05-18 15:43:38 +0000

    something something

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -80,8 +80,8 @@ $(document).ready(function () {
             console.log('waiting for stream');
             call.on('stream', function (stream) {
                 console.log('stream established');
-                var user_li = $('#user-' + call.peer);
-                console.log(user_li);
+                while(document.getElementById('user-' + call.peer) === null){}
+                console.log('user added to userlist');
                 user_li.append($('<audio controls class="hidden userstream" id="audio-' + call.peer + '" src="' + URL.createObjectURL(stream) + '" autoplay=""></audio>'));
                 var stream_controls = $('<div class="stream-controls">');
                 user_li.append(stream_controls);
