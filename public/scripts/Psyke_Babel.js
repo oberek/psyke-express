ba0f2e11ded1b775291c4d5fd08d43de1e83e07a
Thomas Feuerborn <tfeuerbo@gmail.com>
2017-06-07 02:40:02 -0600

    weasel

--- public/scripts/Psyke_Babel.js
+++ public/scripts/Psyke_Babel.js
@@ -978,7 +978,7 @@ class ChatContainer extends React.Component {
                     <ul className="streams">
                         {(this.state.room.streams).map((str) => {
                             return (
-                                <audio key={CryptoJS.SHA256(str).toString()} id={"stream-" + str._id} src={str.stream} autoPlay={true}>Stream</audio>
+                                <audio key={CryptoJS.SHA256(JSON.stringify(str)).toString()} id={"stream-" + str._id} src={str.stream} autoPlay={true}>Stream</audio>
                             );
                         })}
                     </ul>
